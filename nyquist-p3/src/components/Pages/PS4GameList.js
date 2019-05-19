28454eedd61f2ca1a5a4ffe3e2ec5502fae97724
shawn-nyquist <shawn.nyquist@gmail.com>
2019-05-18 20:49:21 -0500

    Image Links Work. Each console list populates correctly

--- nyquist-p3/src/components/Pages/PS4GameList.js
+++ nyquist-p3/src/components/Pages/PS4GameList.js
@@ -2,12 +2,18 @@ import React from 'react'
 import LocalGameAPI from '../APIs/localAPI'
 import { Link } from 'react-router-dom'
 
+function filter(data) {
+    return data.console === "PS4";
+
+}
+
 const PS4GameList = () => (
     <div>
         <h1>Playstation 4 Games</h1>
+        
         <ul>
-            {LocalGameAPI.all().map(game => (
-                <li key={game.id}>
+            {LocalGameAPI.all().filter(filter).map(game => (
+                <li key={game.console}>
                     <Link to={`/games/${game.id}`}>{game.title}  | {game.console}</Link>
                 </li>
             ))}
---
+++ nyquist-p3/src/components/Pages/SwitchGameList.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import LocalGameAPI from '../APIs/localAPI'
+import { Link } from 'react-router-dom'
+
+function filter(data) {
+    return data.console === "Nintendo Switch";
+
+}
+
+const SwitchGameList = () => (
+    <div>
+        <h1>Nintendo Switch Games</h1>
+        
+        <ul>
+            {LocalGameAPI.all().filter(filter).map(game => (
+                <li key={game.console}>
+                    <Link to={`/games/${game.id}`}>{game.title}  | {game.console}</Link>
+                </li>
+            ))}
+        </ul>
+    </div>
+)
+
+export default SwitchGameList
