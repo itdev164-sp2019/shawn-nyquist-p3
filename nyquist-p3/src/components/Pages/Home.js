f7dd9b510e193a97000fac66dcf132b073a6af61
shawn-nyquist <shawn.nyquist@gmail.com>
2019-05-18 21:37:58 -0500

    added hover effects over menu images

--- nyquist-p3/src/components/Pages/Home.js
+++ nyquist-p3/src/components/Pages/Home.js
@@ -10,10 +10,18 @@ import xboxpic from '../images/xbox.jpg'
 
 const Home = () => (
     <div class="btn-grp">
-        <Link to='/games' id="Link"><img src={allgamespic} alt="All Games" width="200px" height="200px"></img></Link>
-        <Link to='/PS4-Games' id ="Link"><img src={ps4pic} alt="PS4 Games" width="200px" height="200px"/></Link>
+        <div class="zoom">
+            <Link to='/games' id="Link"><img src={allgamespic} alt="All Games" width="200px" height="200px"></img></Link>
+        </div>
+        <div class="zoom">
+            <Link to='/PS4-Games' id ="Link"><img src={ps4pic} alt="PS4 Games" width="200px" height="200px"/></Link>
+        </div>
+        <div class="zoom">
         <Link to='/Xbox-Games' id="Link"><img src={xboxpic} alt="Xbox Games" width="200px" height="200px"/></Link>
-        <Link to='/Switch-Games' id="Link"><img src={switchpic} alt="Switch Games" width="200px" height="200px"/></Link>
+        </div>
+        <div class="zoom">
+            <Link to='/Switch-Games' id="Link"><img src={switchpic} alt="Switch Games" width="200px" height="200px"/></Link>
+        </div>    
     </div>
 
 )
